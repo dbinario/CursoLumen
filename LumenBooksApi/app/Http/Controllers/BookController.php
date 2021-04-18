944ca63507466d1c0e293bd127d3626b5a1d8182
Fernando Ruiz <fruiz@wellcom.com.mx>
2021-04-18 16:42:28 -0500

    Se crean metodos los metodos para el controlador de libros

--- LumenBooksApi/app/Http/Controllers/BookController.php
+++ LumenBooksApi/app/Http/Controllers/BookController.php
@@ -19,13 +19,18 @@ class BookController extends Controller
      */
     public function __construct()
     {
-        //
+        
+       
+
     }
 
     
     public function index()
     {
   
+        $books=Book::all();
+
+        return $this->successResponse($books);
 
 
     }
@@ -34,10 +39,31 @@ class BookController extends Controller
     public function store(Request $request)
     {
 
+        $rules=[
+
+            'title'=>'required|max:255',
+            'description'=>'required|max:255',
+            'price'=>'required|min:1',
+            'author_id'=>'required|min:1',
+        ];
+
+        $this->validate($request,$rules);
+
+        $book=Book::create($request->all());
+
+        return $this->successResponse($book,Response::HTTP_CREATED);
+
+
     }
 
     public function show($book)
     {
+
+        
+        $book=Book::findOrFail($book);
+
+        return $this->successResponse($book);
+
 
 
     }
@@ -46,10 +72,50 @@ class BookController extends Controller
     public function update(Request $request,$book)
     {
 
+
+        
+        $rules=[
+
+            'title'=>'max:255',
+            'description'=>'max:255',
+            'price'=>'min:1',
+            'author_id'=>'min:1',
+
+        ];
+
+        $this->validate($request,$rules);
+
+        $book=Book::findOrFail($book);
+
+        $book->fill($request->all());
+
+        if($book->isClean()){
+
+            return $this->errorResponse('At Least one value must change',Response::HTTP_UNPROCESSABLE_ENTITY);
+
+        }else{
+
+            $book->save();
+
+            return $this->successResponse($book);
+
+        }
+
+        
+
+
     }
 
     public function destroy($book)
     {
+
+
+    $book=Book::findOrFail($book);
+
+    $book->delete();
+
+    return $this->successResponse($book);
+
 
 
     }
